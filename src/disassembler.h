d4455c730a65f5612b5f309d4226090418942cc2
paperfox <paperfox56@gmail.com>
2026-06-07 19:47:29 +0100

    Start the port to Zig - Cleaned the workspace

--- src/disassembler.h
+++
@@ -1,8 +0,0 @@
-#ifndef CHIP8_DISASSEMBLER_H
-#define CHIP8_DISASSEMBLER_H
-
-#include <stdint.h>
-int disassemble_opcode(char *buffer, unsigned int size, uint8_t opcode_high,
-                       uint8_t opcode_low);
-
-#endif
--- src/timer.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIMER_H
-#define TIMER_H
-
-long currentTimeMillis();
-
-/** cross-platform sleep function */
-void sleep_ms(int milliseconds);
-
-#endif
